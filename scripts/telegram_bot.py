902d328964edc2deb4be411923b6505b5a87c0e5
darribash <45437051+b4shnhawx@users.noreply.github.com>
2022-06-12 18:34:50 +0200

    Update telegram_bot.py

--- scripts/telegram_bot.py
+++ scripts/telegram_bot.py
@@ -4,13 +4,13 @@ Telegram bot programmed by b4shnhawx.
 https://github.com/b4shnhawx/ssh-telegram-bot
 
 1. Install the dependencies.
-2. Obtain the user id by running the /test command. The user id will be displayed in the terminal where the bot is running.
-3. Enter the API token and your user id (my_chat_id) in the script.
+2. Get the user id by running the /test command. The user id will be displayed in the terminal where the bot is running.
+3. Enter the API token and the user id of the user you want to allow (permit_chat_id) in the script.
 4. Feel free to add or remove commands or functions.
 5. Run the bot!
 
-Note that bots are open to the internet, so any user can start it with their user.
-This bot always compares your user id (my_chat_id) with the user id of the user trying to execute critical commands. If the IDs do not match, it will ignore the commands and warn you that someone has tried to execute a command.
+Note that bots are open to the Internet, so any user can start it with their user.
+This bot always compares the ids of the users allowed to use the bot (permit_chat_id) with the id of the user trying to execute critical commands. If the IDs do not match, it will ignore the commands and warn you that someone has tried to execute a command.
 Commands are exempt from ID checking:
   - /start
   - /test
@@ -26,7 +26,10 @@ import subprocess
 import sys
 import time
 import os
+import paramiko
 
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 app = TeleBot(__name__)
 
@@ -36,50 +39,55 @@ app = TeleBot(__name__)
 
 ### --- VARIABLES ---
 
-token = "1740512324:AAEoYe4DGFPPg_cEc69cBbR5gr0opTUNWGM"
-my_chat_id = "413496441"
+token = "xxxxxxxxx:ABCDEFabcdef..."
+permit_chat_id = (CHAT_ID_1, CHAT_ID_2, CHAT_ID_3...)
 terminal = "inactive"
 
 ### --- FUNCTIONS ---
 
 ###  SEND USER INFO
 
-def info_anon_user(my_chat_id, message):
+def info_anon_user(permited_user, message):
   #Send info about the user. Also try to catch the username and if not, will be excepted.
 	try:
-		app.send_message(my_chat_id, "Name: {} \nUsername: {} \nChat ID: {} \nLanguage: {} \nText: {}".format(message['from']['first_name'], message['from']['username'], message['from']['id'], message['from']['language_code'], message['text']))
+		app.send_message(permited_user, "Name: {} \nUsername: {} \nChat ID: {} \nLanguage: {} \nText: {}".format(message['from']['first_name'], message['from']['username'], message['from']['id'], message['from']['language_code'], message['text']))
 	except:
-		#app.send_message(my_chat_id, "Name: {} {} \nChat ID: {} \nLanguage: {} \nText: {}".format(message['from']['first_name'], message['from']['last_name'], message['from']['id'], message['from']['language_code'], message['text']))
-		app.send_message(my_chat_id, "Name: {} {} \nChat ID: {} \nText: {}".format(message['from']['first_name'], message['from']['last_name'], message['from']['id'], message['text']))
+		#app.send_message(permited_user, "Name: {} {} \nChat ID: {} \nLanguage: {} \nText: {}".format(message['from']['first_name'], message['from']['last_name'], message['from']['id'], message['from']['language_code'], message['text']))
+		app.send_message(permited_user, "Name: {} {} \nChat ID: {} \nText: {}".format(message['from']['first_name'], message['from']['last_name'], message['from']['id'], message['text']))
       
-###  START
+###  START RESTRICTED
 
 @app.route('/start')
 def start(message):
 	chat_id = message['chat']['id']
 
-	if chat_id == int(my_chat_id):
+	if chat_id in permit_chat_id:
 		
 		app.send_message(chat_id, "Bot iniciado")
 		app.send_message(chat_id, "Comandos disponibles (/help): \n" +
 							"/start \n" +
 							"/test \n" +
 							"/ping_pc \n" +
-							"/magic_packet \n" +
+							"/public_ip \n" +
+							"/magic_packet_pc \n" +
+							"/suspend_pc MINUTOS\n" +
+							"/logoff_pc \n" +
+							"/shut_down_pc \n" +
 							"/cmd bash command \n" +
 							"/terminal \n")
 
 	else:
-		app.send_message(chat_id, "Parece que no tienes permiso para acceder :(")
+		app.send_message(chat_id, "Parece que no estas en la whitelist :(")
 		app.send_message(chat_id, "Si realmente quieres usar este bot sigue las instrucciones de mi GitHub para configurarlo por ti mism@:" + 
 						"\n\n" +
 						"https://github.com/b4shnhawx/ssh-telegram-bot")
 
-		#Send info about intrusion
-		app.send_message(my_chat_id, "Alguien ha intentado iniciar sesion en el bot")
-    #Send info about the user
-		info_anon_user(my_chat_id, message)
-      
+		for permited_user in permit_chat_id:
+			#Send info about intrusion
+			app.send_message(permited_user, "Alguien ha intentado iniciar sesion en el bot")
+			#Send info about the user
+			info_anon_user(permited_user, message)
+
 ###  HELP
 
 @app.route('/help')
@@ -90,7 +98,11 @@ def test_chat_id(message):
 							"/start \n" +
 							"/test \n" +
 							"/ping_pc \n" +
-							"/magic_packet \n" +
+							"/public_ip \n" +
+							"/magic_packet_pc \n" +
+							"/suspend_pc \n" +
+							"/logoff_pc \n" +
+							"/shut_down_pc \n" +
 							"/cmd bash command \n" +
 							"/terminal \n")
 
@@ -117,20 +129,24 @@ def terminal_mode_selector(message):
 
 	global terminal
 
-	if chat_id == int(my_chat_id) and terminal == "inactive":
+	if chat_id in permit_chat_id and terminal == "inactive":
 		terminal = "active"
 
-		app.send_message(my_chat_id, "Modo terminal: {}".format(terminal))
+		app.send_message(chat_id, "Modo terminal: {}".format(terminal))
 
-	elif chat_id == int(my_chat_id) and terminal == "active":
+	elif chat_id in permit_chat_id and terminal == "active":
 		terminal = "inactive"
 
-		app.send_message(my_chat_id, "Modo terminal: {}".format(terminal))
+		app.send_message(chat_id, "Modo terminal: {}".format(terminal))
 
 	else:
-		app.send_message(my_chat_id, "Alguien ha intentado EJECUTAR un comando")
-		info_anon_user(my_chat_id, message) 
 		app.send_message(chat_id, "NI. SE. TE. OCURRA. -.-")
+
+		for permited_user in permit_chat_id:
+			#Send info about intrusion
+			app.send_message(permited_user, "Alguien ha intentado EJECUTAR un comando en el bot")
+			#Send info about the user
+			info_anon_user(permited_user, message)
 
 ###  CAPTURE ALL TEXT/COMMANDS
 
@@ -138,25 +154,27 @@ def terminal_mode_selector(message):
 def terminal_mode(message):
 	chat_id = message['chat']['id']
 		
-	if chat_id == int(my_chat_id) and terminal == "active":
+	if chat_id in permit_chat_id and terminal == "active":
 		cmd = message['text']
 		msg = subprocess.run(
 			['bash'], input=cmd, capture_output=True, text=True
 		)
 
-		app.send_message(my_chat_id, msg.stdout)
-		app.send_message(my_chat_id, msg.stderr)
+		app.send_message(chat_id, msg.stdout)
+		app.send_message(chat_id, msg.stderr)
 
-	elif chat_id == int(my_chat_id) and terminal == "inactive":
+	elif chat_id in permit_chat_id and terminal == "inactive":
 		message = "{} :)".format(message['text'])
 		app.send_message(chat_id, message)
 
 	else:
-		app.send_message(my_chat_id, "Alguien ha escrito algo al bot:")
-		info_anon_user(my_chat_id, message)
+		for permited_user in permit_chat_id:
+			app.send_message(permited_user, "Alguien ha escrito algo al bot:")
+			info_anon_user(permited_user, message)
    
-		message = "{} :)".format(message['text'])
-		app.send_message(chat_id, message)
+			message = "{} :)".format(message['text'])
+			app.send_message(permited_user, message)
+		
 
 
 ### ------------------------------------------------
@@ -166,7 +184,9 @@ def terminal_mode(message):
 ### --- VARIABLES ---
 
 mac_magic_packet = "00:D8:61:BF:60:02"
-ip_pc_ping = "192.168.1.10"
+ip_pc = "192.168.1.10"
+user = "NAS"
+password = "WS-dah4909"
 
 ### --- FUNCTIONS ---
 
@@ -176,33 +196,136 @@ ip_pc_ping = "192.168.1.10"
 def ping_pc(message):
 	chat_id = message['chat']['id']
 
-	if chat_id == int(my_chat_id):
+	if chat_id in permit_chat_id:
 		msg = subprocess.run(
-		  ["ping", "-c5", ip_pc_ping], capture_output=True, text=True
+		  ["ping", "-c5", ip_pc], capture_output=True, text=True
 		)
 	
-		app.send_message(my_chat_id, msg.stdout)
-		app.send_message(my_chat_id, msg.stderr)
+		app.send_message(chat_id, msg.stdout)
+		app.send_message(chat_id, msg.stderr)
 
 	else:
 		app.send_message(chat_id, "Que no tienes permisos >:(")
 
+###  PUBLIC IP
+
+@app.route('/public_ip')
+def ping_pc(message):
+        chat_id = message['chat']['id']
+
+        if chat_id in permit_chat_id:
+                msg = subprocess.run(
+                  ["curl", "ifconfig.me"], capture_output=True, text=True
+                )
+
+                app.send_message(chat_id, msg.stdout)
+                app.send_message(chat_id, msg.stderr)
+
+        else:
+                app.send_message(chat_id, "Que no tienes permisos >:(")
+
+
 ###  WAKE ON LAN
 
-@app.route('/magic_packet')
-def magic_packet(message):
+@app.route('/magic_packet_pc')
+def magic_packet_pc(message):
 	chat_id = message['chat']['id']
 
-	if chat_id == int(my_chat_id):
+	if chat_id in permit_chat_id:
 		msg = subprocess.run(
 			["wakeonlan", mac_magic_packet], capture_output=True, text=True
 		)
 
-		app.send_message(my_chat_id, msg.stdout)
-		app.send_message(my_chat_id, msg.stderr)
+		app.send_message(chat_id, msg.stdout)
+		app.send_message(chat_id, msg.stderr)
 
 	else:
 		app.send_message(chat_id, "En serio...?")
+
+###  SUSPEND PC VIA SSH
+
+@app.route('/suspend_pc ?(.*)')
+def suspend_pc(message, time):
+	chat_id = message['chat']['id']
+
+	if time == "":
+		msg = "Ingresa el numero de minutos despues del comando para empezar la cuenta atras"
+
+		app.send_message(chat_id, msg)
+
+	elif chat_id in permit_chat_id:
+
+		time_minutes = int(time) * 60
+		time_hours = int(time) / 60
+
+		msg = "Suspenso programado para dentro de {} horas".format(time_hours)
+		app.send_message(chat_id, msg)
+
+		cmd = 'powershell -command "Start-Sleep -s {}" && powershell -command "%windir%\\System32\\rundll32.exe powrprof.dll,SetSuspendState Hibernate"'.format(time_minutes)
+		print(cmd)
+		
+		ssh.connect(ip_pc, username=user, password=password)
+		stdin, stdout, stderr = ssh.exec_command(cmd)
+		
+		#print(stdout.readlines())
+
+		time.sleep(2)
+
+		
+
+	else:
+		app.send_message(chat_id, "En serio...?")
+
+###  LOGOFF SESSION
+
+@app.route('/logoff_pc')
+def logoff_pc(message):
+	chat_id = message['chat']['id']
+
+	if chat_id in permit_chat_id:
+
+		msg = "Cerrando sesion en el ordenador"
+		
+		cmd = '%windir%\\System32\\shutdown.exe -l'
+		
+		ssh.connect(ip_pc, username=user, password=password)
+		stdin, stdout, stderr = ssh.exec_command(cmd)
+		
+		#print(stdout.readlines())
+
+		time.sleep(2)
+		ssh.close()
+
+		app.send_message(chat_id, msg)
+
+	else:
+		app.send_message(chat_id, "En serio...?")
+
+###  SHUT DOWN PC
+
+@app.route('/shut_down_pc')
+def logoff_pc(message):
+        chat_id = message['chat']['id']
+
+        if chat_id in permit_chat_id:
+
+                msg = "Apagando el ordenador"
+
+                cmd = 'shutdown/s'
+
+                ssh.connect(ip_pc, username=user, password=password)
+                stdin, stdout, stderr = ssh.exec_command(cmd)
+
+                #print(stdout.readlines())
+
+                time.sleep(2)
+                ssh.close()
+
+                app.send_message(chat_id, msg)
+
+        else:
+                app.send_message(chat_id, "En serio...?")
+
 
 ###  SINGLE COMMAND
 
@@ -210,30 +333,31 @@ def magic_packet(message):
 def cmd(message, cmd):
 	chat_id = message['chat']['id']
 
-	if chat_id == int(my_chat_id):
+	if chat_id in permit_chat_id:
 		msg = subprocess.run(
 			['bash'], input=cmd, capture_output=True, text=True
 		)
 
-		app.send_message(my_chat_id, msg.stdout)
-		app.send_message(my_chat_id, msg.stderr)
+		app.send_message(chat_id, msg.stdout)
+		app.send_message(chat_id, msg.stderr)
 
 	else:
-		app.send_message(my_chat_id, "Alguien ha intentado EJECUTAR un comando")
-		app.send_message(my_chat_id, "Name: {} \nUsername: {} \nChat ID: {} \nLanguage: {} \nText: {}".format(message['from']['first_name'], message['from']['username'], message['from']['id'], message['from']['language_code'], message['text'])) 
-		app.send_message(chat_id, "NI. SE. TE. OCURRA. -.-")
+		for permited_user in permit_chat_id:
+			app.send_message(permited_user, "Alguien ha intentado EJECUTAR un comando")
+			app.send_message(permited_user, "Name: {} \nUsername: {} \nChat ID: {} \nLanguage: {} \nText: {}".format(message['from']['first_name'], message['from']['username'], message['from']['id'], message['from']['language_code'], message['text'])) 
+			app.send_message(chat_id, "NI. SE. TE. OCURRA. -.-")
 
 ###  EXAMPLE COMMAND
 
-#@app.route('/command_neme')
+#@app.route('/command_name')
 #def ping_pc(message):
 #	chat_id = message['chat']['id']
 #
-#	if chat_id == int(my_chat_id):
+#	if chat_id in permit_chat_id:
 #		msg = subprocess.run(
 #		  ["COMMAND", "ARG1", "ARG2", "ARG3", "etc"], capture_output=True, text=True
 #		)
-#	
+#
 #		app.send_message(my_chat_id, msg.stdout)
 #		app.send_message(my_chat_id, msg.stderr)
 #
